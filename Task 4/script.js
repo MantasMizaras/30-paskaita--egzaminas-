edb93ad5dfdc16909f66bfe22a3313882f1b3a66
Mantas M <mantasef@gmail.com>
2022-04-05 16:27:54 +0300

    last change

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,29 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+const outputEl = document.getElementById("output");
+
+async function getCars() {
+  const resp = await fetch(ENDPOINT);
+  const data = await resp.json();
+  renderCars(data, outputEl);
+}
+getCars();
+
+function renderCars(carsArr, dest) {
+  dest.innerHTML = "";
+  carsArr.forEach((cObj) => {
+    dest.append(showCars(cObj));
+  });
+}
+
+function showCars(carObj) {
+  const divEl = document.createElement("div");
+  divEl.className = "car-card";
+  divEl.innerHTML = `
+  <h3 class="car-brand">${carObj.brand}</h3>
+  <p class="car-models">${carObj.models.join(", ")}.</p>
+    `;
+  return divEl;
+}
